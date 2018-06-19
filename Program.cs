ad31e1f99236eb2ab75076e68467b71e329db055
nashfisher <nashfisher1991@gmail.com>
2018-06-19 10:33:54 -0500

    update

--- Program.cs
+++ Program.cs
@@ -19,7 +19,10 @@ namespace LPChallenge
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
     }
 }
